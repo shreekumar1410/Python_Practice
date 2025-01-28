3251dc9387515e463b4419c19134d3c078c47e92
shreekumar1410 <shreekumar1410@gmail.com>
2025-01-28 14:58:39 +0530

    FEAT: Continue the Intermediate.

--- Loop_Python/FOR_Loop/Intermediate/program_1.py
+++ Loop_Python/FOR_Loop/Intermediate/program_1.py
@@ -42,5 +42,41 @@ for _ in range(8):
 
 print("Fibonacci sequence:" ,fibonacci_sequence)
 
+#   6.	Write a loop to count the occurrences of a specific character in a string (e.g., count "a" in "banana").
 
+words = "banana"
+count = 0
+
+for char in words:
+    if char == 'a':
+        count += 1
+
+print(f"Count of 'a' in '{words}':", count)
+
+#   7.	Use a loop to find the sum of all elements in an array [10, 20, 30, 40].
+
+array = [10, 20, 30, 40]
+sum = 0
+
+for num in array:
+    sum += num
+
+print("Sum of elements in the array:", sum)
+
+#   8.	Write a loop to check if a number is a palindrome (e.g., 121).
+
+number = 121
+copy_number = number
+reversed_number = 0
+
+for i in str(copy_number):
+    reversed_number = reversed_number * 10 + int(copy_number % 10)
+    copy_number //= 10
+    
+print("Reversed number:", reversed_number)
+
+if number == reversed_number:
+    print(f"{number} is a palindrome.")
+else:
+    print(f"{number} is not a palindrome.")
 
